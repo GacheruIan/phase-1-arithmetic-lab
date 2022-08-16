3c96aeb17167154aae6ea88c18cab533b22e6879
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-08-16 19:51:17 +0300

    Done with this assignment

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+let num1 = 31;
+let num2 = 2;
+function multiply()
+{
+    console.log(multiply = num1*num2);
+}
+multiply();
 
-// Write your code here
+
+let num3 = 22;
+let num4 = 9;
+function mod()
+{
+    console.log(mod = num3%num4);
+}
+mod();
+
+function max()
+{
+    return  max = Math.max(4, 1, 5, 13, 20);
+}
+max();
+
+function random() {
+    
+    return ( random = Math.floor(Math.random() * 10) + 1);
+}
+random();
